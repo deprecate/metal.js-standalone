a935a145996dd1b58b30546f5fabe3f71bc9b1d3
Fernando Souza <fernando.souza@liferay.com>
2016-10-14 18:14:30 -0300

    Up and running

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const metal = require('metal-jsx');
+const Babel = require('babel-standalone');
+const BabelPresetMetalJSX = require('babel-preset-metal-jsx');
+
+const incrementalDOMConfig = BabelPresetMetalJSX.plugins[0][1];
+
+const metalJSX = {
+  plugins: [
+    [require('babel-plugin-incremental-dom').default, incrementalDOMConfig]
+  ]
+};
+
+Babel.registerPreset('metal-jsx', metalJSX);
+window.metal = metal;
