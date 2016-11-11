4a1094ec710e362d754d0eafd0ac0c5dbe319e6b
Maira Bello <maira.araujo@liferay.com>
2016-11-11 15:55:40 -0300

    Fixes JavaScript syntax highlight - Fixes #1
    JSFiddle disables syntax highlight when the Javascript 1.7 language is chosen. If we choose babel it will try to compile it itself though, without the presets we want to use.
    
    I've just found a loophole using a different between the babel code JSFiddle is using and the one we're using. Our code will now change the script type to be `text/babel;harmony=true` instead of just `text/babel`, since that's not supported by JSFiddle's transpiler, but is supported by ours. That way we can guarantee that the compilation is done with our presets, while keeping the syntax highlight.

--- bin/js-fiddle-integration-babel.js
+++ bin/js-fiddle-integration-babel.js
@@ -1,7 +1,7 @@
 (function() {
   var tag = document.querySelector(
-    'script[type="application/javascript;version=1.7"]'
+    'script[type="text/babel"]'
   );
-  tag.setAttribute('type', 'text/babel');
+  tag.setAttribute('type', 'text/babel;harmony=true');
   tag.setAttribute('data-presets', 'es2015,metal-jsx');
 })();
